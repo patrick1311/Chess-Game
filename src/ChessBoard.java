adaddb4e9009eafd0731b81f68665a659e9a33b2
Allen Lam <allenlam465@gmail.com>
2018-10-27 15:01:25 -0700

    Added special moves for Pawn, King, and Rook. Added new class MoveHistory to track previous moves made for special moves.

--- src/ChessBoard.java
+++ src/ChessBoard.java
@@ -1,10 +1,15 @@
+import java.util.Stack;
+
 public class ChessBoard {
 	public static final int TILE_SIZE = 80;
 	public static final int BOARD_LENGTH = 8;
 	
 	private Piece board[][] = new Piece[8][8];
+	private Stack<MoveHistory> previousMoves;
+
 	
 	public Piece[][] getBoard() {
+		previousMoves = new Stack<>();
 		return board;
 	}
 	
@@ -15,6 +20,11 @@ public class ChessBoard {
 		return null;
 	}
 	
+	public MoveHistory getPreviousMove() {
+		return previousMoves.peek();
+	}
+	
+	//Add move onto previousMoves stack
 	public void move(Piece piece, BoardCoordinate tile) {
 		int x = tile.getX();
 		int y = tile.getY();
--- src/Rook.java
+++ src/Rook.java
@@ -1,12 +1,24 @@
 import java.util.List;
 
 public class Rook extends Piece {
+	private boolean hasMoved;
+	
 	public Rook(Player owner, String color) {
 		this.owner = owner;
 		this.color = color;
+		this.setHasMoved(false);
 	}
 
+	public boolean getHasMoved() {
+		return hasMoved;
+	}
+	
+	public void setHasMoved(boolean hasMoved) {
+		this.hasMoved = hasMoved;
+	}
+	
 	public List<BoardCoordinate> accept(ValidMoveVisitor visitor) {
 		return visitor.calculateValidMoves(this);
 	}
+
 }
