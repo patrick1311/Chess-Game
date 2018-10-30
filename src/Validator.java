245b149b5830a739500114600d817273e48ee4e8
Patrick Quach <patrickquach96@gmail.com>
2018-10-29 23:15:17 -0700

    fixed mouseclick laggy condition: moved event listener code to mousePressed to improve responsiveness

--- src/Validator.java
+++ src/Validator.java
@@ -277,8 +277,6 @@ public class Validator implements ValidMoveVisitor {
 			Piece rightRook = board.getPiece(7, row);
 			
 			System.out.println("CASTLING");
-			
-			//System.out.println(emptyBetweenRow(leftRook,king));
 
 			if(leftRook instanceof Rook && isSameColor(king, leftRook) && !((Rook) leftRook).getHasMoved() && emptyBetweenRow(leftRook, king)) {
 				coordinates.add(new BoardCoordinate(x - 2, y));
