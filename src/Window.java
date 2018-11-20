370540b49cfda9fdd199719ffea92fd8699b89bc
Justin Lin <justlin90@gmail.com>
2018-11-20 01:12:16 -0800

    Made frame size independant of OS.

--- src/Window.java
+++ src/Window.java
@@ -1,42 +1,18 @@
 import javax.swing.JFrame;
+import java.awt.Insets;
 
 public class Window {
-	//default border size for all operating systems
-	private static int TOPBORDER = 50;
-	private static int BOTTOMBORDER = 50;
-	private static int LEFTBORDER = 50;
-	private static int RIGHTBORDER = 50;
-	private static String OS = System.getProperty("os.name").toLowerCase();
-
-	public Window(Display game) {
-		if(isWindows()) {
-			TOPBORDER = 26;
-			BOTTOMBORDER = 3;
-			LEFTBORDER = 3;
-			RIGHTBORDER = 3;
-		}
-		else if(isMac()) {
-			TOPBORDER = 22;
-			BOTTOMBORDER = 0;
-			LEFTBORDER = 0;
-			RIGHTBORDER = 0;
-		}
-		//System.out.println(OS);
+	public Window(Display gameDisplay) {
+		final int WINDOW_SIZE = Display.TILE_SIZE * gameDisplay.getGame().getBoard().getBoard().length;
 		JFrame frame = new JFrame("Chess");
-		frame.setSize(Display.TILE_SIZE*8 + LEFTBORDER + RIGHTBORDER
-				,Display.TILE_SIZE*8 + TOPBORDER + BOTTOMBORDER);
-		frame.setLocationRelativeTo(null);
+		
 		frame.setResizable(false);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.add(game);
+		frame.add(gameDisplay);
 		frame.setVisible(true);
-	}
-
-	private boolean isWindows() {
-		return (OS.indexOf("win") >= 0);
-	}
-
-	private boolean isMac() {
-		return (OS.indexOf("mac") >= 0);
+		Insets insets = frame.getInsets();
+		frame.setSize(WINDOW_SIZE + insets.left + insets.right, 
+			WINDOW_SIZE + insets.top + insets.bottom);
+		frame.setLocationRelativeTo(null);
 	}
 }
